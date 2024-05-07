7223b782a87029819754c2778f22d7b4dcd31365
Kaushik7984 <kaushik7984@gmail.com>
2024-05-07 23:48:11 +0530

    final commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div className='font-bold font-serif text-xl text-[#135446]'>MegaBlog.com</div>
+  )
+}
+
+export default Logo
